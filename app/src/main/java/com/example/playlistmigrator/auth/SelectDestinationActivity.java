162f2dbdf6a6228684371545b8a4584389e31b01
Edwin Andres Collazos Lozano <Ecollazoslozano8980@conestogac.on.ca>
2024-03-12 00:41:15 -0400

    A2: adds logic to enable google's OAuth2.0 to be able to use the YouTube's API

---
+++ app/src/main/java/com/example/playlistmigrator/auth/SelectDestinationActivity.java
@@ -0,0 +1,101 @@
+package com.example.playlistmigrator.auth;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.ComponentActivity;
+import androidx.annotation.Nullable;
+
+import com.example.playlistmigrator.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Objects;
+
+public class SelectDestinationActivity extends ComponentActivity {
+    private static final String TAG = SelectDestinationActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 2;
+    private final Context context = SelectDestinationActivity.this;
+    private SignInButton googleBtn;
+    private GoogleSignInClient googleSignInClient;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int[] ints = new int[]{1, 2};
+        setContentView(R.layout.select_destination_activity);
+        googleBtn = findViewById(R.id.google_sign_in);
+        googleBtn.setSize(SignInButton.SIZE_STANDARD);
+        googleBtn.setVisibility(View.INVISIBLE);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope("https://www.googleapis.com/auth/youtube.force-ssl"))
+                .requestScopes(new Scope("https://www.googleapis.com/auth/youtubepartner"))
+                .requestScopes(new Scope("https://www.googleapis.com/auth/youtube")) // Request YouTube scopes
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(context, signInOptions);
+
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(context);
+        updateUI(account);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (null == account) {
+            googleBtn.setVisibility(View.VISIBLE);
+        } else {
+            Log.d(TAG,"user already logged-in");
+            Toast.makeText(context, "User logged in", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
